4b7f374f738d471c884a09cd99b4d710afd4d9ae
Tamara Gell <me@example.com>
2023-01-24 15:20:52 +0100

    Merged master

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,14 +2,17 @@ public class Calculator {
 
     //todo implement add,subtract,multiply and divide
 
-  /*public int add(int a,int b){
 
+  public int add(int a,int b){
+    int result=a+b;
+    return result;
     }
 
     public int subtract(int a,int b){
-
+    int result=a-b;
+    return result;
     }
-*/
+
     public int multiply(int a, int b){
         return (a*b);
     }
