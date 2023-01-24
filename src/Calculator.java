50eddf20f0b226faacf9b8806d12ad206c91065d
Tamara Gell <me@example.com>
2023-01-24 14:56:28 +0100

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+    //todo implement add,subtract,multiply and divide
+
+  /*  public int add(int a,int b){
+
+    }
+
+    public int subtract(int a,int b){
+
+    }
+
+    public int multiply(int a, int b){
+
+    }
+
+    public int divide(int a,int b){
+
+    }*/
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Calculator calculator=new Calculator();
+
+        //Testen der Funktionen
+        /*
+        int result1=calculator.add(1,2);
+        int result2=calculator.subtract(5,3);
+        int result3=calculator.multiply(2,5);
+        int result4=calculator.divide(6,2);
+
+        System.out.println("Summe: "+result1);
+        System.ou.tprintln("Differenz: "+result2);
+        System.out.println("Produkt: "+result3);
+        System.out.println("Quotient: "+result4);
+         */
+    }
+}
